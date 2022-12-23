6b8ae3ebf729a3ca17aba731e485ebe99a91dac7
mrezamazidi <110399448+mrezamazidi@users.noreply.github.com>
2022-12-23 16:59:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ broker = 'broker.mqttdashboard.com'
 port = 1883
 topic1 = 'DY/best_time'
 topic2 = 'DY/energy_saving'
+topic3 = 'DY/end_flag'
 client_id1 = 'myClient'
 client_id2 = 'myClient'
 username = 'mrezamazidi'
@@ -32,11 +33,13 @@ def on_connect(client, userdata, flags, rc):
         print("Failed to connect, return code %d\n", rc)
 
 def on_message(client, userdata, message):
-    global best_time, energy_saving
-    if message.topic ==topic1:
+    global best_time, energy_saving, end_flag
+    if message.topic == topic1:
         best_time = message.payload.decode("utf-8")
-    elif message.topic ==topic2:
+    elif message.topic == topic2:
         energy_saving = message.payload.decode("utf-8")
+    elif message.topic == topic3:
+        end_flag = message.payload.decode("utf-8")
   
 
 
@@ -49,12 +52,14 @@ def index():
     client.loop_start()
     client.subscribe(topic1, 1)
     client.subscribe(topic2, 1)
+    client.subscribe(topic3, 1)
     client.on_message = on_message 
     time.sleep(5)
     client.loop_stop()
 
     flash(f'{best_time}', category = 'category1')
     flash(f'{energy_saving}', category = 'category2')
+    flash(f'{end_flag}', category = 'category3')
     return render_template("index.html")
 
 
